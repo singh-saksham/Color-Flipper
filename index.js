0abb8a6c480fed3b306059b73aad72d5b378ce24
Saksham Singh <70484406+singh-saksham@users.noreply.github.com>
2024-07-23 19:16:06 +0530

    New practice Project (The Color Flipper)

---
+++ index.js
@@ -0,0 +1,14 @@
+const body = document.getElementsByTagName("body")[0];
+
+function setColor(name) {
+    body.style.backgroundColor = name;
+}
+
+function randomColor() {
+    const red = Math.random() * 255;
+    const blue = Math.random() * 255;
+    const green = Math.random() * 255;
+
+    const color = `rgb(${red}, ${green}, ${blue})`;
+    body.style.backgroundColor = color;
+}
